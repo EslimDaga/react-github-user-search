350852554d79883c24c90aaa0fe718bdbc492e2f
Eslim Daga <esliim9811@gmail.com>
2022-04-16 00:28:25 -0500

    A medida que vamos avanzando el proyecto este archivo seguira modificandose ya que aun faltan agregar colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        "color-body-light": "#F6F8FF",
+        "color-body-dark": "#141D2F",
+        "color-primary-light": "#0079FF",
+        "color-primary-dark": "#0079FF",
+        "color-text-primary-light": "#4B6A9B",
+        "color-text-second-light": "#697C9A",
+        "color-text-default-light": "#2B3442",
+      },
       fontFamily: {
         space: ["Space Mono", "monospace"],
+      },
+      boxShadow: {
+        "input-search": "0 16px 30px -25px rgba(70, 96, 187, 20)",
       },
     },
   },
