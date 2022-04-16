530b93c36cf179d1108a3bbfe6b8711eae0c2f34
Eslim Daga <esliim9811@gmail.com>
2022-04-15 19:30:48 -0500

    Configuración para el dark mode, fuente perzonalidada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        space: ["Space Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
