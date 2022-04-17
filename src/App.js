a99beec5e61afc1c51f2a3f8f5f7f5fa16c3f3c7
Eslim Daga <esliim9811@gmail.com>
2022-04-16 19:50:03 -0500

    El titulo esta terminado

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ const App = () => {
     <main className="bg-color-body-light dark:bg-color-body-dark h-screen flex justify-center items-center">
       <div className="max-w-[45.625rem] md:max-h-[30.063rem] lg:max-h-[26.188rem] flex flex-col justify-center gap-9 md:m-auto w-full">
         <div className="flex justify-between h-10 items-center mx-6 sm:mx-6 md:mx-0 lg:mx-0">
-          <h1 className="font-space font-bold text-[1.625rem]">devfinder</h1>
+          <h1 className="font-space font-bold text-[1.625rem] text-color-text-third-light dark:text-white">
+            devfinder
+          </h1>
           <Toggle />
         </div>
         <div className="relative shadow-input-search rounded-[15px] mx-6 sm:mx-6 md:mx-0 lg:mx-0">
